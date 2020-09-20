5303d14c0a5e87d20b36007a2411462f8606bf17
HoSiLuan <luanho192@gmail.com>
2020-09-20 19:24:20 +0700

    create project

---
+++ TinderApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TinderApp
+//
+//  Created by Ho Si Luan on 9/20/20.
+//  Copyright © 2020 Ho Si Luan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ TinderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderApp
+//
+//  Created by Ho Si Luan on 9/20/20.
+//  Copyright © 2020 Ho Si Luan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
